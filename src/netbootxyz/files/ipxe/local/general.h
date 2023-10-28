6e0d64f003d3bd37570ec19ef75437be4ce668cf
andro <97570105+4ndr0666@users.noreply.github.com>
2023-10-28 00:50:28 -0500

    added source files and dir structure

---
+++ src/netbootxyz/files/ipxe/local/general.h
@@ -0,0 +1,20 @@
+#define CERT_CMD              /* Certificate management commands */
+#define CONSOLE_CMD           /* Console command */
+#define DIGEST_CMD            /* Image crypto digest commands */
+#define DOWNLOAD_PROTO_HTTPS  /* Secure Hypertext Transfer Protocol */
+#define IMAGE_COMBOOT         /* COMBOOT */
+#define IMAGE_TRUST_CMD       /* Image trust management commands */
+#define IMAGE_GZIP            /* GZIP image support */
+#define IMAGE_PNG             /* PNG image support */
+#define IMAGE_ZLIB            /* ZLIB image support */
+#define NET_PROTO_IPV6        /* IPv6 protocol */
+#define NET_PROTO_LLDP        /* Link Layer Discovery protocol */
+#define NSLOOKUP_CMD          /* DNS resolving command */
+#define NTP_CMD               /* NTP commands */
+#define PARAM_CMD             /* Form parameter commands */
+#define PCI_CMD               /* PCI commands */
+#define PING_CMD              /* Ping command */
+#define POWEROFF_CMD          /* Power off commands */
+#define REBOOT_CMD            /* Reboot command */
+#define TIME_CMD              /* Time commands */
+#define VLAN_CMD              /* VLAN commands */
